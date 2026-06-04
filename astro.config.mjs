01fdd17f9575f4d21b1f18f7888faa63f5d1613c
bap <baptiste.chenin.dev@gmail.com>
2026-06-04 23:50:51 +0200

    Enhance project configuration by adding Biome support, integrating custom fonts in Astro layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+	adapter: cloudflare(),
+
+	fonts: [
+		{
+			provider: fontProviders.fontsource(),
+			name: "Syne",
+			cssVariable: "--font-syne",
+			weights: ["400 800"],
+			styles: ["normal"],
+			subsets: ["latin"],
+			formats: ["woff2"],
+			fallbacks: ["ui-sans-serif", "sans-serif"],
+		},
+		{
+			provider: fontProviders.fontsource(),
+			name: "Source Serif 4",
+			cssVariable: "--font-source-serif-4",
+			weights: ["100 900"],
+			styles: ["normal", "italic"],
+			subsets: ["latin"],
+			formats: ["woff2"],
+			fallbacks: ["ui-serif", "Georgia", "serif"],
+		},
+	],
 });
